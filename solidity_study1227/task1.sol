abe7c5ea4a62522af59bbba71be124903741d34f
guotao <guotao@canway.net>
2025-12-27 21:11:11 +0800

    '添加文件'

--- solidity_study1227/task1.sol
+++ solidity_study1227/task1.sol
@@ -2,5 +2,11 @@
 pragma solidity ^0.8.0;
 
 contract Voting {
-    
+    mapping (string => uint) public vote_map;
+    string[] public candidateList;
+
+    function vote(string memory candidateName) public pure {
+        
+    }
+
 }
