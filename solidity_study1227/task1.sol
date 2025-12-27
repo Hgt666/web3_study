4e47cf636a6fe8ea9096aa2d83b0571659d1f6b4
guotao <guotao@canway.net>
2025-12-27 21:16:34 +0800

    '11'

--- solidity_study1227/task1.sol
+++ solidity_study1227/task1.sol
@@ -5,8 +5,15 @@ contract Voting {
     mapping (string => uint) public vote_map;
     string[] public candidateList;
 
-    function vote(string memory candidateName) public pure {
+    // 添加候选人
+    function addCandidate(string memory Name) public pure {
+        require(bytes(Name).length==0,"name is not null");
+        // 判断候选人是否已存在
         
+    }
+
+    function vote(string memory candidateName) public pure {
+
     }
 
 }
